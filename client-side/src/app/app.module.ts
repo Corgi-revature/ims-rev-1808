8ddf5600f5154d7068d2206765feb6612b2caa01
Chen Sia <cheneus@gmail.com>
2018-09-24 01:10:52 -0400

    init -- test login and based UI template is ready, package_json is updated to so that there is no security issue, keep in mind that the template has a vendor css do not use vendor css(class-name)

---
+++ client-side/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { AgmCoreModule } from '@agm/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routes.service';
+
+import { ViewsModule } from './views/views.module';
+import { SharedModule } from './shared/shared.module';
+import { ErrorModule } from './views/errors/error.module';
+
+// main layout
+import { NavigationModule } from './main-layout/navigation/navigation.module';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    NavigationModule,
+    AppRoutes,
+    RouterModule,
+    FormsModule,
+    SharedModule,
+    ViewsModule,
+    ErrorModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
