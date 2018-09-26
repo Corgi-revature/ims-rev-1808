f0abae4ff4721ea322b7e18df4336214f80dd80c
djvelez <djvelez@gmail.com>
2018-09-26 16:04:08 -0400

    beans and DAOs

--- IMS/src/main/java/com/revature/data/DeliveryDAO.java
+++ IMS/src/main/java/com/revature/data/DeliveryDAO.java
@@ -7,8 +7,8 @@ import com.revature.beans.Delivery;
 public interface DeliveryDAO {
 	public Delivery addDelivery(Delivery deli);
 	public Delivery getDeliveryById(int id);
-	public Set<Delivery> getDeliverysCriteria();
-	public Set<Delivery> getDeliverys();
+	public Set<Delivery> getDeliveriesCriteria();
+	public Set<Delivery> getDeliveries();
 	public void updateDelivery(Delivery deli);
 	public void deleteDelivery(Delivery deli);
 	public void deleteDeliveryById(int id);
---
+++ IMS/src/main/java/com/revature/data/ItemHibernate.java
@@ -0,0 +1,51 @@
+package com.revature.data;
+
+import java.util.Set;
+
+import com.revature.beans.Item;
+
+public class ItemHibernate implements ItemDAO {
+
+	@Override
+	public Item addItem(Item ite) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item getItemById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Item> getItemsCriteria() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Item> getItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateItem(Item ite) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteItem(Item ite) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteItemById(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
