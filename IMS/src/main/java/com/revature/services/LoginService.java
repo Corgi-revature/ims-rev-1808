b1e81ee83641e818b425a7ad527db9dd4bbabf46
Chen Sia <cheneus@gmail.com>
2018-09-26 20:36:05 -0400

    added aspect

--- IMS/src/main/java/com/revature/services/LoginService.java
+++ IMS/src/main/java/com/revature/services/LoginService.java
@@ -1,7 +1,15 @@
 package com.revature.services;
 
+import java.util.Set;
+
 import com.revature.beans.User;
 
 public interface LoginService {
-	User login(String email, String password);
+	public int addUser(User user);
+	public User getUserById(int id); 
+	public Set<User> getUsersCriteria();
+	public Set<User> getUsers();
+	public User getUserLogin(String email, String password);
+	public void updateUser(User user);
+	public void deleteUser(User user);
 }
