408605bebd70e53267e88fd4ca3fc21df75bdf13
Steven Hang <stevenhang.main@gmail.com>
2018-10-01 09:46:19 -0400

    forgot to import service

--- IMS/src/main/java/com/revature/services/InventoryServiceImpl.java
+++ IMS/src/main/java/com/revature/services/InventoryServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.revature.beans.Inventory;
 import com.revature.data.InventoryDAO;
