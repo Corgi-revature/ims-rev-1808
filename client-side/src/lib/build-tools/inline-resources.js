ee4c78ad2ff72e6faefa43f5bb7c0df5de806a5c
Chen <cheneus@gmail.com>
2018-10-01 22:00:49 -0400

    update to eval

--- client-side/src/lib/build-tools/inline-resources.js
+++ client-side/src/lib/build-tools/inline-resources.js
@@ -37,6 +37,7 @@ function inlineTemplate(fileContent, filePath) {
 }
 
 function inlineStyles(fileContent, filePath) {
+  fileContent =  fileContent.toString();
   return fileContent.replace(/styleUrls:\s*(\[[\s\S]*?])/gm, (_match, styleUrlsValue) => {
     // The RegExp matches the array of external style files. This is a string right now and
     // can to be parsed using the `eval` method. The value looks like "['AAA.css', 'BBB.css']"
