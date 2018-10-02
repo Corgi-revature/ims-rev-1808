6a1c8e97a5f0d7cfa4d866b2315d9f60e72dace9
Chen <cheneus@gmail.com>
2018-10-01 21:21:38 -0400

    Update ItemHibernate.java
    Arrangement

--- IMS/src/main/java/com/revature/data/ItemHibernate.java
+++ IMS/src/main/java/com/revature/data/ItemHibernate.java
@@ -5,11 +5,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.hibernate.query.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import org.hibernate.query.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
