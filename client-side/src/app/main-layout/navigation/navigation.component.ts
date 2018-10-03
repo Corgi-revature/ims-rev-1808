881ff4c3544431947e3e54daede198084f7aab52
Chen <cheneus@gmail.com>
2018-10-03 00:39:06 -0400

    added customer dash template

--- client-side/src/app/main-layout/navigation/navigation.component.ts
+++ client-side/src/app/main-layout/navigation/navigation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../login/auth.service';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { UserService } from '../../login/user/user.service';
 
 
 @Component({
@@ -11,14 +12,16 @@ import { Router } from '@angular/router';
 })
 export class NavigationComponent implements OnInit {
   isLoggedIn$: Observable<boolean>;
+  isAdmin$: Observable<boolean>;
   clicked: boolean;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private userService: UserService, private router: Router) {
     this.clicked = this.clicked === undefined ? false : true;
   }
 
   ngOnInit() {
     this.isLoggedIn$ = this.authService.isLoggedIn;
+    this.isAdmin$ = this.userService.isAdmin;
     this.authService.checkLogin();
   }
 
