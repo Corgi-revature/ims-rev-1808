01750f0c3e1e41fb20ce544ff1fbbf9b63695e67
Chen <cheneus@gmail.com>
2018-10-04 13:27:37 -0400

    java entity for view

--- IMS/src/main/java/com/revature/beans/Order.java
+++ IMS/src/main/java/com/revature/beans/Order.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
