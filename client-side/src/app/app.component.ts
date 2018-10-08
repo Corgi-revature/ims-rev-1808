f79dd2104b0cd6412d61f3b39a6f4ba21321d2d3
Chen <cheneus@gmail.com>
2018-10-07 20:24:17 -0400

    Update to add auth0 app token
    Yet to integrate with current login method

--- client-side/src/app/app.component.ts
+++ client-side/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Auth0Service } from './shared/services/auth0/auth0.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,8 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'Corgi-IMS';
+
+  constructor(public auth: Auth0Service) {
+    auth.handleAuthentication();
+  }
 }
